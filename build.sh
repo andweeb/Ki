640468e02530f8f83b33738e411b6b977d22a2a7
Andrew Kwon <andrewshky@gmail.com>
2018-05-28 17:06:37 -0700

    Print to console for commands run in build script

--- build.sh
+++ build.sh
@@ -9,12 +9,15 @@ DIST="$CWD/dist"
 
 # Generate docs
 hs -c "hs.doc.builder.genJSON(\"$CWD\")" | grep -v "^--" > docs.json
+echo "build.sh: Generated Ki docs"
 
 # Create or clear out dist folder
 if [ -d "$DIST" ] && [ ! -z "$DIST" ]; then
     rm -rfv "${DIST:?}"/*
+    echo "build.sh: Cleared out content in dist folder"
 else
     mkdir "$DIST"
+    echo "build.sh: Created dist folder"
 fi
 
 # Copy lua scripts and generated docs from root directory to folder
@@ -22,11 +25,14 @@ fi
 if [ -f "$CWD/docs.json" ]; then
     cp "$CWD/docs.json" "$DIST/docs.json"
 fi
+echo "build.sh: Copied lua scripts and docs to dist folder"
 
 # Remove pre-existing spoon link
 if [ -d "$LINK_TARGET" ]; then
     rm -rv "$LINK_TARGET"
+    echo "build.sh: Removed pre-existing Ki spoon"
 fi
 
 # Symlink dist folder to spoons folder
 ln -sf "$DIST" "$LINK_TARGET"
+echo "build.sh: Symlinked dist folder to spoons folder"
