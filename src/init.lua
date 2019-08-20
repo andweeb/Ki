c4295ce39bc7dbdd9c57e959e9753b87918b19a2
Andrew Kwon <andrewshky@gmail.com>
2019-08-20 13:25:34 -0700

    Bump version to 1.6.1

--- src/init.lua
+++ src/init.lua
@@ -44,7 +44,7 @@ local Ki = {}
 Ki.__index = Ki
 
 Ki.name = "Ki"
-Ki.version = "1.6.0"
+Ki.version = "1.6.1"
 Ki.author = "Andrew Kwon"
 Ki.homepage = "https://github.com/andweeb/ki"
 Ki.license = "MIT - https://opensource.org/licenses/MIT"
